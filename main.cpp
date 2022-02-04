2277d75801999e2468fa016eccc515dae32595f7
Roman Ravas <romorko@gmail.com>
2022-02-04 12:37:47 +0100

    Doplnena metoda in a qsort

--- main.cpp
+++ main.cpp
@@ -5,22 +5,24 @@ int main()
 {
     Pole<int,3> moje;
     moje.push(1);
-    moje.push(3);
+    moje.push(13);
     moje.push(5);
     moje.push(8);
     moje.push(10);
     moje.push(11);
-    moje.push(12);
-    moje.push(13);
+    moje.push(2);
+    moje.push(19);
     moje.push(14);
-    moje.push(20);
+    moje.push(0);
     moje.push(21);
-    moje.push(22);
+    moje.push(2);
     moje.push(23);
-    moje.push(24);
+    moje.push(16);
     moje.push(25);
     moje[3]=44;
-    //moje.printPole();
-   std::cout<<moje;
+    std::cout<<moje<<std::endl;
+    moje.sort('d');
+    std::cout<<moje<<std::endl;
+   //std::cout<<moje.in(14);
     return 0;
 }
--- pole.h
+++ pole.h
@@ -6,6 +6,9 @@
 template <typename  Any=int, unsigned pocet=10>
 class Pole;
 
+template <typename T>
+int comp(const void * a, const void *b);
+
 template <typename  Any, unsigned int pocet>
 std::ostream & operator<<(std::ostream & ,const Pole <Any,pocet> &);
 
@@ -20,12 +23,18 @@ private:
 public:
     Pole();
     ~Pole(); //destruktor uvolni pamat po zaniknuti objektu
-    Pole(unsigned kolko);
+    explicit Pole(unsigned kolko);
     Any &operator[](unsigned index);
-    Any operator[](const unsigned index) const;
+    Any operator[](unsigned index) const;
     bool push(Any hodnota);
+    bool in(Any hladam) const;//zisti ci je prvok v poli
+    Any max() const;
+    Any min() const;
+    void sort(char ako='a'); //implicitne vzostupne
     void printPole() const;
     friend std::ostream & operator<< <>(std::ostream &,const Pole<Any,pocet> &);
+    static int cmpA(const void * a, const void *b);
+    static int cmpD(const void * a, const void *b);
 };
 
 template<typename Any, unsigned int pocet>//vytvorim implicitne velke pole
@@ -60,7 +69,7 @@ Any &Pole<Any, pocet>::operator[](unsigned int index)
 }
 
 template<typename Any, unsigned int pocet> //pretazeny operator aby sa dalo priamo pristupovat k prvkom array
-Any Pole<Any, pocet>::operator[](const unsigned int index) const
+Any Pole<Any, pocet>::operator[](unsigned int index) const
 {
     return array[index];
 }
@@ -106,6 +115,55 @@ std::ostream &operator<< (std::ostream &os, const Pole<Any,pocet>& other)
     return os;
 }
 
+template<typename Any, unsigned int pocet>
+bool Pole<Any, pocet>::in(Any hladam) const
+{
+    for(int i=0;i<curIndex;++i)
+    {
+        if(hladam==(*this)[i])
+        {
+            return true;
+        }
+    }
+    return false;
+}
 
+template<typename Any, unsigned int pocet>
+int Pole<Any, pocet>::cmpD(const void *a, const void *b)
+{
+   Any * prvy= (Any *)a;
+   Any * druhy= (Any *)b;
+   return (*prvy)<(*druhy)?1:(*prvy)>(*druhy)?-1:0;
+}
+
+template<typename Any, unsigned int pocet>
+int Pole<Any, pocet>::cmpA(const void *a, const void *b)
+{
+    Any * prvy= (Any *)a;
+    Any * druhy= (Any *)b;
+    return (*prvy)<(*druhy)?-1:(*prvy)>(*druhy)?1:0;
+}
+
+template<typename Any, unsigned int pocet>
+void Pole<Any, pocet>::sort(char ako)
+{
+    if(ako=='a')
+    {
+        qsort(array,curIndex, sizeof(array[0]),Pole<Any,pocet>::cmpA);
+    }
+    else
+    {
+        qsort(array,curIndex, sizeof(array[0]),Pole<Any,pocet>::cmpD);
+    }
+    //qsort(array,curIndex, sizeof(array[0]),comp<Any>);
+}
+
+template <typename T>
+int comp(const void * a, const void *b)
+{
+    T * prvy= (T *)a;
+    T * druhy= (T *)b;
+    return (*prvy)<(*druhy)?1:(*prvy)>(*druhy)?-1:0;
+}
 
 #endif //POLE_POLE_H
